19954fa5dfe6d1256238b470bc074f8adff6ca3c
jorenezra <tackeedot@gmail.com>
2014-12-12 14:25:01 +0800

    updated till paver

--- Bank/test/unit/account_test.py
+++ Bank/test/unit/account_test.py
@@ -1,9 +1,6 @@
 import unittest
 from bank.account import Account
 
-
-
-
 class TestAccount(unittest.TestCase):
     def test_account_object_returns_current_balance(self):
         account = Account("001", 50)
